1af16141c7d1b3070c7c44ec54d6c6e02eebd5e3
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2025-05-05 17:46:46 +0530

    Created uilt.py

---
+++ src/utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def compute_returns_to_go(rewards, gamma=0.99):
+    rtg = np.zeros_like(rewards, dtype=float)
+    running = 0.0
+    for i in reversed(range(len(rewards))):
+        running = rewards[i] + gamma * running
+        rtg[i] = running
+    return rtg
+
+def simple_logger(log_dict, step):
+    print(f"Step {step}: " + ", ".join(f"{k}={v:.3f}" for k,v in log_dict.items()))
