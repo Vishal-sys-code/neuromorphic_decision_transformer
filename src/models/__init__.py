9e3fd6c2eae46d981258cab1ff06fca9a729c845
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2025-05-06 18:06:20 +0530

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,6 @@
 """Models package for SpikingMindRL."""
 
-from .simple_decision_transformer import SimpleDecisionTransformer
+# Commented out until the module is implemented
+# from .simple_decision_transformer import SimpleDecisionTransformer
 
-__all__ = ["SimpleDecisionTransformer"] 
+__all__ = [] # Removed SimpleDecisionTransformer from __all__ 
