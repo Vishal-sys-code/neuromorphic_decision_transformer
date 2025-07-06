e957eced645cb760f4ce02379d9e6f1185365384
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2025-07-06 15:48:09 +0530

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -5,3 +5,13 @@
 
 # __all__ = [] # Removed SimpleDecisionTransformer from __all__ 
 from .spiking_layers import SpikingSelfAttention
+
+# This file makes 'models' a Python sub-package.
+# You can also use it to control what is imported when someone does 'from .models import *'
+# For example:
+# from .snn_dt import SNNDT
+# from .positional_spike_encoder import PositionalSpikeEncoder
+# from .dendritic_routing import DendriticRouter
+
+# Making them available for easier import if desired:
+# __all__ = ['SNNDT', 'PositionalSpikeEncoder', 'DendriticRouter']
